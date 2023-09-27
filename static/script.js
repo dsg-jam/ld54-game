2a27b0c445aa0836d5be83e02de3dd4a6caa00bf
mineiwik <29404520+mineiwik@users.noreply.github.com>
2023-09-27 21:10:35 +0200

    initial commit

---
+++ static/script.js
@@ -0,0 +1,26 @@
+(async () => {
+  const response = await fetch(
+    "https://api.github.com/repos/dsg-jam/ld54-game/contents/?ref=gh-pages"
+  );
+  const data = await response.json();
+
+  let htmlString = "";
+  let paths = [];
+
+  data.sort((a, b) => b.name.localeCompare(a.name));
+
+  for (let file of data) {
+    if (file.type === "dir" && /^\d/.test(file.name)) {
+      paths.push(file.path);
+      htmlString += `<span><a href="${file.path}">${file.name}</a></span><br/>`;
+    }
+  }
+
+  if (paths.length > 0) {
+    htmlString =
+      `<span><a href="${paths.sort().reverse()[0]}">Latest</a></span><br/>` +
+      htmlString;
+  }
+
+  document.getElementsByClassName("dsg-links")[0].innerHTML = htmlString;
+})();
