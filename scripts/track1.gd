a5f7665758343d1121ded83813de4b7a32fbbd8e
mineiwik <29404520+mineiwik@users.noreply.github.com>
2023-09-30 17:14:47 +0200

    add demo track

---
+++ scripts/track1.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var car_count = 8
+
+@onready var train_vehicle_reference = load("res://prefabs/TrainVehicle.tscn")
+@onready var engine = $TrainEngine
+
+func _setup_train():
+	#engine.connect("train_info", $TestWorld, "update_train_info")
+	engine.add_to_track($Tracks/Track)
+	
+	var last_car = engine
+	for index in range(car_count):
+		var car = train_vehicle_reference.instantiate()
+		add_child(car)
+		last_car.set_follower_car(car)
+		last_car = car
+
+func _on_Timer_timeout():
+	_setup_train()
